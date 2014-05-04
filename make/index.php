dad29a46a24735068b7fa87a5f2b19e5516c6056
honghu91 <honghu91@hotmail.com>
2014-05-05 00:45:09 +0800

    add make

---
+++ make/index.php
@@ -0,0 +1,54 @@
+﻿<?php
+include 'markdown.php';
+
+//scanfiles
+$dir = "../mdfile/";
+
+$files = scandir($dir);
+
+$fileArr = array();
+
+foreach($files as $file){
+    if($file == "." || $file == "..") continue;
+
+
+    if(stripos($file, "swp") > 0) continue;
+
+    $tmp = explode("-", $file);
+
+    if($tmp[1]){
+        $fileArr[$tmp[1]] = $file;
+    }
+}
+
+krsort($fileArr);
+
+$result = array("list" => array());
+
+foreach($fileArr as $key => $val){
+    $time = str_replace(".md", "", $key);
+    $title = str_replace("-" . $key, "", $val);
+
+    $file = file_get_contents($dir . $val);
+
+    $id = uniqid();
+
+    $brief_str = substr($file, 0, 150) . "..";
+
+    $mks = Markdown($file);
+
+    $list = array(
+        "title" => iconv("gb2312", "utf-8", $title),
+        "pubtime" => $time,
+        "brief" => Markdown($brief_str),
+        "id" => $id
+    );
+
+    $result['list'][] = $list;
+}
+
+$fileContent = json_encode($result);
+if(file_put_contents("../data/content.json", $fileContent)){
+    echo "ok";
+}
+//$data = Markdown(file_get_contents("../mdfile/test.md"));
